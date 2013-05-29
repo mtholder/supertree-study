feac7be82deba6c255956ac8b049e0363e4e59c3
Patrick Niehaus <niehaupr@email.sc.edu>
2013-05-29 15:55:46 -0500

    input bug fix

--- taxonomyToNewick.py
+++ taxonomyToNewick.py
@@ -1,30 +1,52 @@
+import sys
 class Taxon(object):
     def __init__(self,name,parent):
         self.name = name
         self.parent = parent
         self.children = []
-
-keyToObject = {}
-#root = none
-
-
-def parse_ott(filename,splitchar ='\t|\t'):
+        
+    def write_as_newick(self, out):
+        if self.children:
+            out.write('(')
+            for n,c in enumerate(self.children):
+                c.write_as_newick(out)
+                out.write(')')
+            out.write(self.name)
+        return out
+                
+def parse_ott(filename = 'ott2-taxo-first500.txt',splitchar ='\t|\t'):
     '''
-    function to be used
+    function to be used..
+    filename is based on user input, but is also
+    given by a default value.
+    
+    splitchar is given a default value; however,
+    this can be changed via function input.
     '''
-    inp_file = open(filename, 'rU')
-    inp = iter(inp_file)
-    next(inp)
-   # size = len(file.readlines())
-    #print(size)
-    for row in inp:
-        row_list = row.split(splitchar)
-        t_id = int(row_list[0])
-        name = row_list[2]
-        parent_id = int(row[1])
-        parent = keyToObject.get(parent_id)
-        taxon = Taxon(name, parent)
-        keyToObject[t_id] = taxon
-        if parent is not None:
-            parent.children.append(taxon)
-    inp_file.close()
+    root = None
+    keyToObject = {}
+    with open(filename, 'rU') as inp_file:
+        inp = iter(inp_file)
+        next(inp)
+        for row in inp:
+            row_list = row.split(splitchar)
+            t_id = int(row_list[0])
+            name = row_list[2]
+            try:
+                parent_id = int(row_list[1])
+                parent = keyToObject.get(parent_id)
+            except:
+                parent = None
+            taxon = Taxon(name, parent)
+            keyToObject[t_id] = taxon
+            if root is None:
+                root = taxon
+        
+            if parent is not None:
+               # print('parent of ' + t_id + ' is ' + parent.name)
+                parent.children.append(taxon)
+            else:
+                pass
+               # print('No parent for ' + name + 'looked up ' + str(parent_id))
+                #print(str(keyToObject))
+    return root
