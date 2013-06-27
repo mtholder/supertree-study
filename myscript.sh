f866405064eeb4f5829040dfc9b143e2e6cb53e8
Mark T. Holder <mtholder@gmail.com>
2013-06-27 15:00:22 -0500

    paup commands appended

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #load taxonomy and convert to newick format. 
+set -x
+
 taxonomy_file="$1"
 newick_taxonomy="primates_ott_newick1.txt"
 taxonomyToNewick.py "${taxonomy_file}" > "${newick_taxonomy}" || exit
@@ -24,16 +26,16 @@ do
 		time python ~/supertree-study/MRP_Matrix_Generator.py myoutput/run$i/primates_big_tree_TAG$i.txt > myoutput/run$i/primates_MRP$i.txt || exit
 		#Eventually run MRP file through PAUP to return a nexus file readable by dendropy..
 		# Not working correctly, need to find a way to run all commands through the terminal...
-		time paup -n myoutput/run$i/primates_MRP$i.txt -l myoutput/run$i/primates_MRP$i || exit
+		echo "HSearch;" >> myoutput/run$i/primates_MRP$i.txt
+		echo "SaveTrees file = myoutput/run${i}/primates_MRP${i}_out.txt ;" >> myoutput/run$i/primates_MRP$i.txt
+		
+
+		time paup -n myoutput/run$i/primates_MRP$i.txt || exit
 		#time paup -r myoutput/run$i/primates_MRP$i.txt -l myoutput/run$i/primates_MRP$i || exit	
-		#GenerateTrees || exit
-		#RootTrees || exit
-		#time SaveTrees || exit
-		#quit || exit
 		#Convert to Nexon for TAG algorithm.
-		time python ~/supertree-study/newick_to_nexon.py myoutput/run$i/primates_big_tree_TAG$i.txt > myoutput/run$i/primates_bt_nexon$i.txt || exit
+		#time python ~/supertree-study/newick_to_nexon.py myoutput/run$i/primates_big_tree_TAG$i.txt > myoutput/run$i/primates_bt_nexon$i.txt || exit
 		#Create bogus ott for TAG.
-		time python ~/supertree-study/bogus_ott_gen.py "${newick_taxonomy}" > myoutput/run$i/primates_bogus_ott$i.txt || exit
+		#time python ~/supertree-study/bogus_ott_gen.py "${newick_taxonomy}" > myoutput/run$i/primates_bogus_ott$i.txt || exit
 		#Run TAG algorithm (as Nexson) compare to bogus ott generated.
 
 		#set primates1.db to automatically delete each time to avoid confusion.
@@ -54,7 +56,7 @@ do
 		
 		#SAS Algorithm.
 		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar inittax myoutput/run$i/primates_bogus_ott$i.txt ~/Dropbox/holder_lab/supertrees/primate_taxonomy_with_synonyms/primates_synonyms.txt myoutput/run$i/primates$i.db || exit
-java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar addtaxonomymetadatanodetoindex 3 myoutput/run$i/primates$i.db || exit
+		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar addtaxonomymetadatanodetoindex 3 myoutput/run$i/primates$i.db || exit
 		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar listsources myoutput/run$i/primates$i.db || exit
 		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar pgloadind myoutput/run$i/primates$i.db myoutput/run$i/primates_bt_nexon$i.txt || exit
 		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar synthesizedrafttreelist 805080 _0_0 myoutput/run$i/primates$i.db || exit
