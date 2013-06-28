7b63c6b3a476499a3318106149ae9ff2a1e3dc96
Patrick Niehaus <patrick@patrick-Aspire-M5-581T.(none)>
2013-06-28 12:43:49 -0500

    updated shell script

--- myscript.sh
+++ myscript.sh
@@ -1,68 +1,77 @@
 #!/bin/sh
 #load taxonomy and convert to newick format. 
 set -x
-
+if test -z $TREEMACHINE_ROOT 
+then
+	echo "Treemachine root must be defined"
+	exit 1
+fi
+treemachine_jar="$TREEMACHINE_ROOT/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+if ! test -f "$treemachine_jar"
+then
+	echo "Treemachine jar not found"
+	exit 1
+fi
 taxonomy_file="$1"
-newick_taxonomy="primates_ott_newick1.txt"
-taxonomyToNewick.py "${taxonomy_file}" > "${newick_taxonomy}" || exit
+synonyms_file="$2"
+newick_taxonomy="primates_ott_newick.txt"
+primates_bigtree="primates_big_tree.txt" # fake 'true' tree. has ottolids attached.
+primates_MRP="primates_MRP.txt"
+primates_bigtree_nexon="primates_bt_nexon.txt"
+primates_bogus_ott="primates_bogus_ott.txt"
+primates_SAS_tree="primates_SAS_tree.tre"
+primates_MRP_tree="primates_MRP_tree.tre"
+distance_results="distances.txt"
+original_dir="$PWD"
+
+taxonomyToNewick.py "${taxonomy_file}" > "$original_dir/${newick_taxonomy}" || exit
 
 if test -d myoutput
 then
-	echo "myoutput is in the way !"
+	echo "myoutput is in the way!"
 	exit 1
 fi
+mkdir myoutput
+cd myoutput
 
 for i in $(seq 1 1 10) # Parameter swoop begin
 do
+	mkdir run$i
 	#for j in $(seq 1 1 1000) # probably will have to replace i with j in sequence's later..
 	#do
-		#Set each folder to automatically delete..
-		mkdir myoutput/run$i
 		
 
 		#Run taxonomy through big-tree.
-		time ~/big-tree-collection-simulator/src/big-tree-sim-0.0.2a -c ~/big-tree-collection-simulator/test/basic-commands.txt "${newick_taxonomy}" > myoutput/run$i/primates_big_tree_TAG$i.txt || exit # fake 'true' tree output here. 
+		big-tree-sim-0.0.2a -c "$original_dir/basic-commands.txt" "$original_dir/${newick_taxonomy}" > run$i/"${primates_bigtree}" || exit # fake 'true' tree output here. 
 		#Run MRP algorithm (MRP output seems to be off still, no '?' appear... Is it neccessary to get rid of ottolids in names?.
-		time python ~/supertree-study/MRP_Matrix_Generator.py myoutput/run$i/primates_big_tree_TAG$i.txt > myoutput/run$i/primates_MRP$i.txt || exit
+		MRP_Matrix_Generator.py "run$i/${primates_bigtree}" > run$i/"${primates_MRP}" || exit
 		#Eventually run MRP file through PAUP to return a nexus file readable by dendropy..
-		# Not working correctly, need to find a way to run all commands through the terminal...
-		echo "HSearch;" >> myoutput/run$i/primates_MRP$i.txt
-		echo "SaveTrees file = myoutput/run${i}/primates_MRP${i}_out.txt ;" >> myoutput/run$i/primates_MRP$i.txt
-		
-
-		time paup -n myoutput/run$i/primates_MRP$i.txt || exit
-		#time paup -r myoutput/run$i/primates_MRP$i.txt -l myoutput/run$i/primates_MRP$i || exit	
+		echo "HSearch;" >> run$i/"${primates_MRP}"
+		echo "SaveTrees file = run$i/${primates_MRP_tree} ;" >> "run$i/${primates_MRP}"
+		time paup -n "run$i/${primates_MRP}"
 		#Convert to Nexon for TAG algorithm.
-		#time python ~/supertree-study/newick_to_nexon.py myoutput/run$i/primates_big_tree_TAG$i.txt > myoutput/run$i/primates_bt_nexon$i.txt || exit
+		newick_to_nexon.py run$i/"${primates_bigtree}" > run$i/"${primates_bigtree_nexon}" || exit
 		#Create bogus ott for TAG.
-		#time python ~/supertree-study/bogus_ott_gen.py "${newick_taxonomy}" > myoutput/run$i/primates_bogus_ott$i.txt || exit
+		bogus_ott_gen.py "$original_dir/${newick_taxonomy}" > run${i}/"${primates_bogus_ott}" || exit
 		#Run TAG algorithm (as Nexson) compare to bogus ott generated.
 
 		#set primates1.db to automatically delete each time to avoid confusion.
-		if test -d myoutput/run$i/primates$i.db
+		if test -d run$i/primates$i.db
 		then
 			echo "primates$i.db is in the way! removing now.."
-			rm -r myoutput/primates$i.db
+			rm -r run$i/primates$i.db
 		fi
 
-		#if test -z "$TRACE_LEVEL"
-		#then
-		#	config_arg_val=debuglog4j.properties
-		#else
-		#	config_arg_val=tracelog4j.properties
-		#fi
-
-		#set -x
 		
 		#SAS Algorithm.
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar inittax myoutput/run$i/primates_bogus_ott$i.txt ~/Dropbox/holder_lab/supertrees/primate_taxonomy_with_synonyms/primates_synonyms.txt myoutput/run$i/primates$i.db || exit
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar addtaxonomymetadatanodetoindex 3 myoutput/run$i/primates$i.db || exit
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar listsources myoutput/run$i/primates$i.db || exit
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar pgloadind myoutput/run$i/primates$i.db myoutput/run$i/primates_bt_nexon$i.txt || exit
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar synthesizedrafttreelist 805080 _0_0 myoutput/run$i/primates$i.db || exit
-		java -jar ~/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar extractdrafttree 805080 myoutput/run$i/primates_tree$i.tre myoutput/run$i/primates$i.db || exit
+		java -jar "$treemachine_jar" inittax run$i/"${primates_bogus_ott}" "$synonyms_file" run$i/primates$i.db || exit
+		java -jar "$treemachine_jar" addtaxonomymetadatanodetoindex 3 run$i/primates$i.db || exit
+		java -jar "$treemachine_jar" listsources run$i/primates$i.db || exit
+		java -jar "$treemachine_jar" pgloadind run$i/primates$i.db run$i/"${primates_bigtree_nexon}" || exit
+		java -jar "$treemachine_jar" synthesizedrafttreelist 805080 _0_0 run$i/primates$i.db || exit
+		java -jar "$treemachine_jar" extractdrafttree 805080 run$i/"${primates_SAS_tree}" run$i/primates$i.db || exit
 
-		#Compare MRP and TAG distance.
-		time python ~/supertree-study/distance.py myoutput/run$i/primates_big_tree_TAG$i.txt myoutput/run$i/primates_MRP$i.tre myoutput/run$i/primates_tree$i.tre > myoutput/run$i/symmetric_differences$i.txt || exit
+		#Compare MRP and TAG distance. Have to do something about single quote..
+		#distance.py run$i/"${primates_bigtree}" run$i/"${primates_MRP_tree}" run$i/"${primates_SAS_tree}" > run$i/"${distance_results}" || exit
 	#done
 done
