0e0a3bde6a94ab0a752b1d74d17fee8a2e8f3c46
Patrick Niehaus <patrick@patrick-Aspire-M5-581T.(none)>
2013-07-02 14:48:35 -0500

    myscript update

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#load taxonomy and convert to newick format. 
+
 set -x
 if test -z $TREEMACHINE_ROOT 
 then
@@ -12,18 +12,23 @@ then
 	echo "Treemachine jar not found"
 	exit 1
 fi
+#declare variables
 taxonomy_file="$1"
 synonyms_file="$2"
 newick_taxonomy="primates_ott_newick.txt"
-primates_bigtree="primates_big_tree.txt" # fake 'true' tree. has ottolids attached.
+primates_big_tree="primates_big_tree.txt" 
+primates_true_tree_ids="primates_true_tree_ids.txt"
+primates_true_tree_wo_ids="primates_true_tree_no_ids.txt"
+primates_input_trees="primates_input_trees.txt"
 primates_MRP="primates_MRP.txt"
-primates_bigtree_nexon="primates_bt_nexon.txt"
+primates_input_trees_nexon="primates_input_trees_nexon.txt"
 primates_bogus_ott="primates_bogus_ott.txt"
 primates_SAS_tree="primates_SAS_tree.tre"
 primates_MRP_tree="primates_MRP_tree.tre"
 distance_results="distances.txt"
 original_dir="$PWD"
 
+#load taxonomy and convert to newick format. 
 taxonomyToNewick.py "${taxonomy_file}" > "$original_dir/${newick_taxonomy}" || exit
 
 if test -d myoutput
@@ -34,7 +39,7 @@ fi
 mkdir myoutput
 cd myoutput
 
-for i in $(seq 1 1 10) # Parameter swoop begin
+for i in $(seq 1 1 10) # Parameter swoop begin.
 do
 	mkdir run$i
 	#for j in $(seq 1 1 1000) # probably will have to replace i with j in sequence's later..
@@ -42,20 +47,27 @@ do
 		
 
 		#Run taxonomy through big-tree.
-		big-tree-sim-0.0.2a -c "$original_dir/basic-commands.txt" "$original_dir/${newick_taxonomy}" > run$i/"${primates_bigtree}" || exit # fake 'true' tree output here. 
-		#Run MRP algorithm (MRP output seems to be off still, no '?' appear... Is it neccessary to get rid of ottolids in names?.
-		MRP_Matrix_Generator.py "run$i/${primates_bigtree}" > run$i/"${primates_MRP}" || exit
+		big-tree-sim-0.0.2a -c "$original_dir/basic-commands.txt" "$original_dir/${newick_taxonomy}" > run$i/"${primates_big_tree}" || exit 
+		head -n1 run$i/"${primates_big_tree}" > run$i/"${primates_true_tree_ids}" || exit
+		nl=$(wc -l run$i/"${primates_true_tree_ids}") || exit
+		ninp=$(expr $n1-1) || exit
+		tail -n$ninp run$i/"${primates_big_tree}" > run$i/"${primates_input_trees}" || exit
+
+		name_split.py run$i/"${primates_true_tree_ids}"> run$i/"${primates_true_tree_wo_ids}" || exit
+
+		#Run MRP algorithm (MRP output seems to be off still, no '?' appear... 
+		MRP_Matrix_Generator.py "run$i/${primates_input_trees}" > run$i/"${primates_MRP}" || exit
 		#Eventually run MRP file through PAUP to return a nexus file readable by dendropy..
 		echo "HSearch;" >> run$i/"${primates_MRP}"
 		echo "SaveTrees file = run$i/${primates_MRP_tree} ;" >> "run$i/${primates_MRP}"
 		time paup -n "run$i/${primates_MRP}"
 		#Convert to Nexon for TAG algorithm.
-		newick_to_nexon.py run$i/"${primates_bigtree}" > run$i/"${primates_bigtree_nexon}" || exit
+		newick_to_nexon.py run$i/"${primates_input_trees}" > run$i/"${primates_input_trees_nexon}" || exit
 		#Create bogus ott for TAG.
 		bogus_ott_gen.py "$original_dir/${newick_taxonomy}" > run${i}/"${primates_bogus_ott}" || exit
 		#Run TAG algorithm (as Nexson) compare to bogus ott generated.
 
-		#set primates1.db to automatically delete each time to avoid confusion.
+		#set primates[1-10].db to automatically delete each time to avoid confusion.
 		if test -d run$i/primates$i.db
 		then
 			echo "primates$i.db is in the way! removing now.."
@@ -67,11 +79,11 @@ do
 		java -jar "$treemachine_jar" inittax run$i/"${primates_bogus_ott}" "$synonyms_file" run$i/primates$i.db || exit
 		java -jar "$treemachine_jar" addtaxonomymetadatanodetoindex 3 run$i/primates$i.db || exit
 		java -jar "$treemachine_jar" listsources run$i/primates$i.db || exit
-		java -jar "$treemachine_jar" pgloadind run$i/primates$i.db run$i/"${primates_bigtree_nexon}" || exit
+		java -jar "$treemachine_jar" pgloadind run$i/primates$i.db run$i/"${primates_input_trees_nexon}" || exit
 		java -jar "$treemachine_jar" synthesizedrafttreelist 805080 _0_0 run$i/primates$i.db || exit
 		java -jar "$treemachine_jar" extractdrafttree 805080 run$i/"${primates_SAS_tree}" run$i/primates$i.db || exit
 
-		#Compare MRP and TAG distance. Have to do something about single quote..
-		#distance.py run$i/"${primates_bigtree}" run$i/"${primates_MRP_tree}" run$i/"${primates_SAS_tree}" > run$i/"${distance_results}" || exit
+		#Compare MRP and TAG distance. Have to do something about single quote..sln make name 'first''name'
+		distance.py run$i/"${primates_true_tree_wo_ids}" run$i/"${primates_MRP_tree}" run$i/"${primates_SAS_tree}" > run$i/"${distance_results}" || exit
 	#done
-done
+done #Parameter swoop end.
